7b790253a4e5bb5a1eba90c841b85281afcb13dc
ABhsk <abhsk05@gmail.com>
2017-06-08 11:51:40 +0530

    Add some dummy endpoints

--- src/main/java/com/sample/app/AppApplication.java
+++ src/main/java/com/sample/app/AppApplication.java
@@ -2,11 +2,19 @@ package com.sample.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+	}
+
+	@RequestMapping("/dummy")
+	public String dummyHandler(){
+		return "you have reached dummyland";
 	}
 }
